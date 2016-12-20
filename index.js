89d4504e28a8eb820811140f3c81dc2a4aacdc73
tuubu <chasosantenpe@gmail.com>
2016-12-20 06:19:47 +0000

    ログから日付を削除

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 const http = require('http');
 const jade = require('jade');
 const server = http.createServer((req, res) => {
-  const now = new Date();
-  console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+  console.info('Requested by ' + req.connection.remoteAddress);
   res.writeHead(200, {
     'Content-Type': 'text/html',
     'charset': 'utf-8'
@@ -35,7 +34,7 @@ const server = http.createServer((req, res) => {
     case 'POST':
       req.on('data', (data) => {
         const decoded = decodeURIComponent(data);
-        console.info('[' + now + '] 投稿: ' + decoded);
+        console.info(' 投稿: ' + decoded);
         res.write('<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8"></head><body><h1>' +
           decoded + 'が投稿されました</h1></body></html>');
         res.end();
@@ -46,11 +45,11 @@ const server = http.createServer((req, res) => {
   }
 
 }).on('error', (e) => {
-  console.error('[' + new Date() + '] Server Error', e);
+  console.error(' Server Error', e);
 }).on('clientError', (e) => {
-  console.error('[' + new Date() + '] Client Error', e);
+  console.error(' Client Error', e);
 });
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
-  console.info('[' + new Date() + '] Listening on ' + port);
+  console.info(' Listening on ' + port);
 });
